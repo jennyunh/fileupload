f51c1d91413e96ddc8c0fdcff2321651e0461213
Jenny Hwang <jennyunh@gmail.com>
2022-10-05 18:08:56 -0700

    screw multer. Using express-fileupload instead.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const fileUpload = require('express-fileupload');
 
 const userRoutes = require('./routes/users');
 const db = require('./data/database');
@@ -10,10 +11,23 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+/*the urlencoded middleware worked with the default form encryption type:
+application/x-www-form-urlencoded 
+but it doesnt work with multipart/form-data
+Express does not have an available middleware for it.
+Use thirdparty package like express-fileupload
+*/ 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(fileUpload());
+
+
+
 
 app.use(userRoutes);
+
+
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+
+
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -9,5 +11,33 @@ router.get('/', function(req, res) {
 router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
+
+
+
+//use express-fileupload package 
+router.post('/upload', function(req, res) {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.sampleFile;
+  uploadPath = __dirname + '/req.body.username/' + sampleFile.name;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+      console.log("sample file is: " + sampleFile + " uploadPath is : " + uploadPath)
+    res.send('File uploaded!');
+  });
+});
+
+
+
 
 module.exports = router;
